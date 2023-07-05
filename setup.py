b79bdaaeb3147e1e0c309b6e40f9a1aec2249895
Krishna <krishankushwah0111@gmail.com>
2023-07-05 19:58:48 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = 'cellSegmentation',
+    version = '0.0.0',
+    author = 'Krishna',
+    author_email = 'krishankushwah0112@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+
+)
